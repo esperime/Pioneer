3b988f1d93d34b4bf845bad2cd8c58783f4fe17a
ki-bun <159973401+ki-bun@users.noreply.github.com>
2026-05-04 00:07:43 +0800

    chore: add navigation compose dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -90,4 +90,6 @@ dependencies {
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
+    val nav_version = "2.9.8"
+    implementation("androidx.navigation:navigation-compose:$nav_version")
 }
