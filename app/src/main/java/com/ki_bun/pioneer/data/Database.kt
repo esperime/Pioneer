8da5c98e6c197a0c467e45c23edfdcb4d746e602
ki-bun <159973401+ki-bun@users.noreply.github.com>
2026-05-10 23:46:44 +0800

    chore: add schema directory for database and prepare migration for the implementation of image and tags

--- app/src/main/java/com/ki_bun/pioneer/data/Database.kt
+++ app/src/main/java/com/ki_bun/pioneer/data/Database.kt
@@ -1,11 +1,21 @@
 package com.ki_bun.pioneer.data
 
 import android.content.Context
+import androidx.room.AutoMigration
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 
-@Database(entities = [Item::class], version = 1)
+@Database(
+    entities = [Item::class],
+    version = 2,
+    exportSchema = true
+)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun itemDao(): ItemDao
     companion object {
@@ -15,10 +25,19 @@ abstract class AppDatabase : RoomDatabase() {
             return INSTANCE ?: synchronized(this) {
                 val instance = Room.databaseBuilder(context.applicationContext,
                     AppDatabase::class.java,
-                    "app_database").build()
+                    "app_database")
+                    .addMigrations(MIGRATION_1_2)
+                    .build()
                 INSTANCE = instance
                 instance
             }
         }
+    }
+}
+
+val MIGRATION_1_2 = object : Migration(1,2) {
+    override fun migrate(db: SupportSQLiteDatabase) {
+        db.execSQL("ALTER TABLE items ADD COLUMN tags TEXT NOT NULL DEFAULT ''")
+        db.execSQL("ALTER TABLE items ADD COLUMN imagePath TEXT")
     }
 }
