09e690e6e0ac94b430a5a39cc3b3f83661289bf4
ki-bun <159973401+ki-bun@users.noreply.github.com>
2026-05-11 12:57:50 +0800

    chore(release): 2.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,8 +44,8 @@ android {
         applicationId = "com.ki_bun.pioneer"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.2.1"
+        versionCode = 7
+        versionName = "2.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
